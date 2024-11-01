ab5f54899a54d6c3fac6d4ecc98f0832ebcabcaf
Ethan Pineda <joshuapineda66@gmail.com>
2024-10-31 19:20:20 -0500

    list of products done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const dummyProducts = [
+    {
+      id: 1,
+      name: "Boba Milk Tea",
+      price: 8.99,
+      image_url:
+        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
+    },
+    {
+      id: 2,
+      name: "Boba Milk Tea",
+      price: 8.99,
+      image_url:
+        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
+    },
+    {
+      id: 3,
+      name: "Boba Milk Tea",
+      price: 8.99,
+      image_url:
+        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
+    },
+    {
+      id: 4,
+      name: "Boba Milk Tea",
+      price: 8.99,
+      image_url:
+        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ume Products</h1>
+      <div className="products">
+        {dummyProducts.map((product) => (
+          <div className="product" key={product.id}>
+            <img src={product.image_url} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>${product.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
