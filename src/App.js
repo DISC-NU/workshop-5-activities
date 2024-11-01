faf8a04bb5762c7bf40a4853de875b943609e37e
Ethan Pineda <joshuapineda66@gmail.com>
2024-10-31 20:02:38 -0500

    activirty 2 done

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
 import "./App.css";
-
+import Products from "./components/Products/Products.jsx";
+import DetailedProduct from "./components/DetailedProduct/DetailedProduct.jsx";
 import React from "react";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  const dummyProducts = [
-    {
-      id: 1,
-      name: "Boba Milk Tea",
-      price: 8.99,
-      image_url:
-        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
-      description: "A delicious milk tea with boba",
-    },
-    {
-      id: 2,
-      name: "Boba Milk Tea",
-      price: 8.99,
-      image_url:
-        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
-      description: "A delicious milk tea with boba",
-    },
-    {
-      id: 3,
-      name: "Boba Milk Tea",
-      price: 8.99,
-      image_url:
-        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
-      description: "A delicious milk tea with boba",
-    },
-    {
-      id: 4,
-      name: "Boba Milk Tea",
-      price: 8.99,
-      image_url:
-        "https://hpxezonhbbsjcymbdafz.supabase.co/storage/v1/object/public/disc-product-images/product-images/1730133510983-n58q2c.jpeg",
-      description: "A delicious milk tea with boba",
-    },
-  ];
-
   return (
-    <div>
-      <h1>Ume Products</h1>
-      <div className="products">
-        {dummyProducts.map((product) => (
-          <div className="product" key={product.id}>
-            <img src={product.image_url} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>${product.price}</p>
-            <p>{product.description}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Hello World</h1>
+            </>
+          }
+        />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<DetailedProduct />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/DetailedProduct/DetailedProduct.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function DetailedProduct() {
+  const [product, setProduct] = useState(null);
+
+  const { productId } = useParams();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const response = await fetch(
+        "https://disc-product-demo-api.onrender.com/api/products/" + productId
+      );
+      const data = await response.json();
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
+
+  return (
+    <div>
+      {product ? (
+        <div>
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
+          <img src={product.image_url} />
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
